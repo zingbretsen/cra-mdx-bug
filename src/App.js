45a739cec6cd5a07b1250262eaf40f69caa62760
Zach Ingbretsen <zach.ingbretsen@datarobot.com>
2022-03-14 11:42:05 -0400

    adds broken mdx example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable import/no-webpack-loader-syntax */
+import Content from '!@mdx-js/loader!./content.mdx'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  return <Content />
 }
-
-export default App;
